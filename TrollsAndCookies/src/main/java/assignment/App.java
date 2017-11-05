9c633802781ac3bbb0d1f5689ab2b3ab71479b8d
rsb.bth <rsb.bth@gmail.com>
2017-11-05 18:17:11 +0100

    Updated thread initiation, synchronization to be updated

--- TrollsAndCookies/src/main/java/assignment/App.java
+++ TrollsAndCookies/src/main/java/assignment/App.java
@@ -22,14 +22,14 @@ public class App implements Runnable
     //N-2 Cookies - 8
     //Hammer hits
     //=>Recursion until Trolls = 1
-    static  int no_of_trolls;
-    int  no_of_cookies = no_of_trolls -1;
+
+    int  no_of_cookies = 4;
     Map cookieMap, gameMap;
     int round = 1;
 
 
     void trollsAndCookies(int no_of_trolls) throws InterruptedException {
-        //no_of_cookies = no_of_trolls-1;
+        no_of_cookies = no_of_trolls-1;
         System.out.println(no_of_cookies);
         Random random_time = new Random();
         int time_in_millis = random_time.nextInt(100);
@@ -37,9 +37,9 @@ public class App implements Runnable
             //Random time 1000 seconds
             Thread[] threads = new Thread[no_of_trolls];
             for (int i = 0; i < threads.length; i++) {
-                threads[i] = new Thread(new App());
+                threads[i] = new Thread(this);
                 threads[i].setName(i+"");
-               // threads[i].sleep(time_in_millis);
+                threads[i].sleep(time_in_millis);
                 threads[i].start();
             }
             round++;
@@ -59,22 +59,23 @@ public class App implements Runnable
 
     public void run(){
 
-        int curr_thread = Integer.parseInt(Thread.currentThread().getName());
-        if(no_of_cookies > 0){
-            grabCookie(round, curr_thread,no_of_cookies);
-            no_of_cookies--;
-            System.out.println("Troll "+ curr_thread + " Grabs the cookie ! and " + no_of_cookies + " are up for grabs !");
+            int curr_thread = Integer.parseInt(Thread.currentThread().getName());
+            if(no_of_cookies > 0){
+                grabCookie(round, curr_thread,no_of_cookies);
+                no_of_cookies--;
+                System.out.println("Troll "+ curr_thread + " Grabs the cookie ! and " + no_of_cookies + " are up for grabs !");
 
-        }else{
-            System.out.println("Troll "+ curr_thread + " is Out !");
+            }else{
+                System.out.println("Troll "+ curr_thread + " is Out !");
 
+            }
         }
 
-    }
 
 
     public static void main( String[] args ) throws InterruptedException {
 
+        int no_of_trolls;
         System.out.println("Enter size of Trolls : \n");
         Scanner trolls =  new Scanner(System.in);
         no_of_trolls = trolls.nextInt();
