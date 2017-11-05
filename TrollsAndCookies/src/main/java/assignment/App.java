29da59c2443eaf91ac083c36f17ce8335865b964
rsb.bth <rsb.bth@gmail.com>
2017-11-05 15:47:29 +0100

    Round added to Map

--- TrollsAndCookies/src/main/java/assignment/App.java
+++ TrollsAndCookies/src/main/java/assignment/App.java
@@ -24,14 +24,15 @@ public class App implements Runnable
     //=>Recursion until Trolls = 1
 
     int  no_of_cookies;
-    Map cookieMap;
+    Map cookieMap, gameMap;
+    int round = 1;
 
     void trollsAndCookies(int no_of_trolls) throws InterruptedException {
         while(no_of_trolls > 1 ){
             no_of_cookies = no_of_trolls-1;
             //Random time 1000 seconds
             Random random_time = new Random();
-            int time_in_millis = random_time.nextInt(1000);
+            int time_in_millis = random_time.nextInt(1);
 
             Thread[] threads = new Thread[no_of_trolls];
             for (int i = 0; i < threads.length; i++) {
@@ -40,21 +41,24 @@ public class App implements Runnable
                 threads[i].sleep(time_in_millis);
                 threads[i].start();
             }
-
+            round++;
             trollsAndCookies(no_of_trolls -1 );
         }
 
     }
 
-    void grabCookie(int troll, int cookie){
+    void grabCookie(int round, int troll, int cookie){
         cookieMap = new HashMap();
-        cookieMap.put(cookie, troll);
+        cookieMap.put(troll, cookie);
+        gameMap = new HashMap();
+        gameMap.put(round, cookieMap);
+
    }
 
     public void run(){
         int curr_thread = Integer.parseInt(Thread.currentThread().getName());
         if(no_of_cookies > 0){
-            grabCookie(curr_thread,no_of_cookies);
+            grabCookie(round, curr_thread,no_of_cookies);
             no_of_cookies--;
             System.out.println("Troll "+ curr_thread + " Grabs the cookie ! and " + no_of_cookies + " are up for grabs !");
 
